babb7de58a920b24b44c350f16d40d63db190bb9
Danish <danish.danish.danish2016@gmail.com>
2023-01-04 18:26:53 +0500

    Added login signup UI

---
+++ src/components/Signup.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <div class="text-center text-4xl text-gray-700 font-light pb-10">
+      Signup
+    </div>
+    <div class="text-gray-700 font-light pb-2 pt-4">Name</div>
+    <input
+      v-model="name"
+      type="text"
+      class="w-full border border-gray-300 rounded-md p-3"
+      placeholder="Enter your name"
+    />
+    <div class="text-gray-700 font-light pb-2 pt-4">Email</div>
+    <input
+      v-model="email"
+      type="text"
+      class="w-full border border-gray-300 rounded-md p-3"
+      placeholder="Enter your email"
+    />
+    <div class="text-gray-700 font-light pb-2 pt-4">Password</div>
+    <input
+      v-model="password"
+      type="password"
+      class="w-full border border-gray-300 rounded-md p-3"
+      placeholder="Enter your password"
+    />
+    <div class="text-gray-700 font-light pb-2 pt-4">Confirm Password</div>
+    <input
+      v-model="confirmPassword"
+      type="password"
+      class="w-full border border-gray-300 rounded-md p-3"
+      placeholder="Enter your password"
+    />
+
+    <button
+      @click="signup"
+      class="w-full bg-[#25D366] text-white rounded-md p-3 mt-4"
+    >
+      Signup
+    </button>
+  </div>
+</template>
+<script setup>
+import { useUserStore } from '@/store/user-store';
+import { useRouter } from 'vue-router';
+import { ref } from 'vue';
+
+const emit = defineEmits(['signup']);
+const userStore = useUserStore();
+const router = useRouter();
+let email = ref('');
+let password = ref('');
+let name = ref('');
+let confirmPassword = ref('');
+
+const emailPattren = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+// const emailPattren = /[a-zA-Z]+@gmail\.[a-zA-Z]+/;
+const verifyEmail = () => {
+  return emailPattren.test(email.value);
+};
+const verifyPassword = () => {
+  return password.value.length > 6;
+};
+const signup = () => {
+  if (verifyEmail()) {
+    if (verifyPassword()) {
+      //   console.log('login');
+      //   console.log(email.value, password.value);
+      //   email.value = '';
+      //   password.value = '';
+      emit('signup', email.value, password.value);
+    } else {
+      alert('Please enter a valid password');
+    }
+  } else {
+    alert('Please enter a valid email');
+  }
+  //   setTimeout(() => {
+  //     router.push('/');
+  //   }, 200);
+};
+</script>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,38 +1,64 @@
 <template>
-    <div class="w-full">
-        <div class="bg-teal-600 z-[-1] w-full h-[225px] fixed top-0"></div>
-        <div class="bg-[#191919] z-[-1] w-full h-[calc(100vh-225px)] fixed bottom-0"></div>
+  <div class="w-full">
+    <div class="bg-teal-600 z-[-1] w-full h-[225px] fixed top-0"></div>
+    <div
+      class="bg-[#191919] z-[-1] w-full h-[calc(100vh-225px)] fixed bottom-0"
+    ></div>
 
-        <div class="max-w-xl mx-auto">
-            <div class="mt-10 flex items-center w-full">
-                <img width="40" src="whatsapp-logo.png" alt="">
-                <div class="font-semibold text-gray-100 ml-6">WHATSAPP WEB</div>
-            </div>
+    <div class="max-w-xl mx-auto">
+      <div class="mt-10 flex items-center w-full">
+        <img width="40" src="whatsapp-logo.png" alt="" />
+        <div class="font-semibold text-gray-100 ml-6">WHATSAPP WEB</div>
+      </div>
 
-            <div class="bg-white z-10 p-24 m-6 mt-10">
-                <div class="text-center text-4xl text-gray-700 font-light pb-10">
-                    WhatsApp Clone
-                </div>
-                <div class="w-full flex justify-center bg-[#191919] p-3 rounded-md">
-                    <GoogleLogin :callback="callback"/>
-                </div>
+      <div class="bg-white rounded-md z-10 px-20 pt-10 pb-10 m-6 mt-10">
+        <div>
+          <Signup v-if="showSignup" />
+          <Login v-else @login="login" />
+          <div class="pt-2">
+            <div v-if="showSignup">
+              Already have an account?
+              <span @click="showSignup = false">Login</span>
             </div>
+            <div v-else>
+              Dont have an account?
+              <span @click="showSignup = true">Signup</span>
+            </div>
+          </div>
+          <div class="text-gray-700 font-light pb-2 pt-4 my-2 text-center">
+            or continue with google
+          </div>
         </div>
+        <div class="w-full flex justify-center bg-[#191919] p-3 rounded-md">
+          <GoogleLogin :callback="callback" />
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script setup>
-import { useUserStore } from '@/store/user-store'
-import { useRouter } from 'vue-router'
-const userStore = useUserStore()
-const router = useRouter()
+import Login from '@/components/Login.vue';
+import { useUserStore } from '@/store/user-store';
+import { useRouter } from 'vue-router';
+import { ref } from 'vue';
+import Signup from '../components/Signup.vue';
+const userStore = useUserStore();
+const router = useRouter();
+let showSignup = ref(false);
+let email = ref('');
+let password = ref('');
+
+const login = (email, password) => {
+  console.log(email, password);
+};
 
 const callback = async (response) => {
-    await userStore.getUserDetailsFromGoogle(response)
-    setTimeout(() => { router.push('/') }, 200)
-}
+  await userStore.getUserDetailsFromGoogle(response);
+  setTimeout(() => {
+    router.push('/');
+  }, 200);
+};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
